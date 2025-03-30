6e213e2b4ab189b58390d1295da2eeb7910c249a
ad_sekime <s.sekime@adseed.co.jp>
2025-03-30 17:47:55 +0900

    add: crx.jsインストール

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    crx({ manifest }),
+  ],
   server: {
     host: true, // ローカルサーバーを外部からアクセス可能にする
     port: 5173, // 開発サーバーが使用するポート番号
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      host: 'localhost', // ホストマシンのホスト名またはIPアドレス
+  },
+  },
+  legacy: {
+    skipWebSocketTokenCheck: true,
   },
 });
